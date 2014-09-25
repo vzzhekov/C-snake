2603f0a46148dd270315f969ca31cabf04f036fc
Vilian Zhekov <vzzhekov@gmail.com>
2014-09-25 17:30:11 +0300

    FinalSnake
    saf

--- JustConsoleSnake II/JustConsoleSnake II/Program.cs
+++ JustConsoleSnake II/JustConsoleSnake II/Program.cs
@@ -15,7 +15,7 @@ namespace JustConsoleSnake_II
         {
             SetUpConsole.SetField();
 
-
+            int threadSleep = 100;
             
             Position[] directions = new Position[] {
                 new Position(0,1), //right
@@ -27,13 +27,15 @@ namespace JustConsoleSnake_II
             Queue<Position> snakeElements = new Queue<Position>();
             for (int i = 0; i < 6; i++)
             {
-                snakeElements.Enqueue(new Position(0, i));
+                snakeElements.Enqueue(new Position(1, i));
             }
             int score = 0;
 
+
+
             Random randomNumberGenerator = new Random();
             Position food = new Position(randomNumberGenerator.Next(0, Console.WindowHeight), randomNumberGenerator.Next(0, Console.WindowWidth));
-
+           
             int direction = 0;
 
             while (true)
@@ -49,19 +51,38 @@ namespace JustConsoleSnake_II
                 Position nextDirection = directions[direction];
                 Position snakeNewHead = new Position((snakeHead.Col + nextDirection.Col), nextDirection.Row + snakeHead.Row);
                 snakeElements.Enqueue(snakeNewHead);
+
+                if (snakeNewHead.Row < 0 || snakeNewHead.Col < 0 || snakeNewHead.Row >= Console.WindowWidth || snakeNewHead.Col >= Console.WindowHeight)
+                {
+
+                    SetUpConsole.SetCursor(10,10);
+                    Console.WriteLine("Game Over");
+
+                    return;
+
+                }
+                
+                
+                
+                
+                
                 if (snakeNewHead.Col == food.Col && snakeNewHead.Row == food.Row)
                 {
 
 
-                    food = new Position(randomNumberGenerator.Next(0, Console.WindowHeight), randomNumberGenerator.Next(0, Console.WindowWidth));
+                    food = new Position(randomNumberGenerator.Next(2, Console.WindowHeight-1), randomNumberGenerator.Next(2, Console.WindowWidth));
                     score++;
+                    threadSleep= threadSleep - 4;
                 }
+
+
 
                 else
                 {
 
                     snakeElements.Dequeue();
                 }
+
 
 
                 Console.Clear();
@@ -71,10 +92,11 @@ namespace JustConsoleSnake_II
                 }
 
                 PrintOnScreen.PrintWithPosition(food, "@");
+               
 
-                PrintOnScreen.PrintWithInts(Console.WindowWidth/2,0,"Score: "  + score.ToString());
+                PrintOnScreen.PrintWithInts(Console.WindowWidth/3,0,"Score: "  + ((snakeElements.Count() -6)*10).ToString());
 
-                int threadSleep = 100;
+                
                 Thread.Sleep(threadSleep);
 
             }
